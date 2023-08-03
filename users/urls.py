f58ac39d626a093d4ab09e7a7f290fa1e639b681
Sam <iaiznura@gmail.com>
2023-08-03 19:56:07 +0300

    add users urls

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from users.views import UserCreateView
+
 # from lms.views.course import *
 # from lms.views.lesson import *
 # from lms.views.payment import *
@@ -9,6 +11,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user-create/', UserCreateView.as_view(), name='create_user'),
 ]
 #
 # router = routers.SimpleRouter()
