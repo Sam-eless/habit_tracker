c7dc41c9121e15f9d6bee178997ab50928c34f24
Sam <iaiznura@gmail.com>
2023-08-03 23:24:13 +0300

    add pleasant_habit to admin panel

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from habits.models import PleasantHabit
+
+
 # Register your models here.
+
+@admin.register(PleasantHabit)
+class PleasantHabitAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "place", "time", "action", "frequency", "execution_time", "is_public", "is_active",)
