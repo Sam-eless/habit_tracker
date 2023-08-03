a4f45eb177f5f6864c6f2a79986bd5c8891b82d8
Sam <iaiznura@gmail.com>
2023-08-03 19:56:26 +0300

    upd user serializer

--- users/user_serializer.py
+++ users/user_serializer.py
@@ -9,4 +9,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ["id", "email", "first_name", "last_name", "tg_login", "tg_chat_id"]
