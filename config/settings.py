6ede90f35dd2005cac493ce44da603d8e5f5b148
Sam <iaiznura@gmail.com>
2023-08-03 19:55:49 +0300

    add django_filters to settings.py

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
+
     'rest_framework_simplejwt',
     'users',
 ]
