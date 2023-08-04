1fe95c701c77254bd1356f7a3968057a65b87476
Sam <iaiznura@gmail.com>
2023-08-04 22:51:03 +0300

    add validator to serializers pleasant habit

--- habits/serializers/pleasant_habit.py
+++ habits/serializers/pleasant_habit.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 from habits.models import GoodHabit, PleasantHabit
+from habits.validators import ExecutionTimeValidator
 
 
 class PleasantHabitSerializer(serializers.ModelSerializer):
-    # good_habit = SlugRelatedField(slug_field='pk', queryset=GoodHabit.objects.all())
-    # validators = [UrlValidator(field="url")]
+    validators = [ExecutionTimeValidator(field="execution_time")]
 
     class Meta:
         model = PleasantHabit
