c7c6922faf593175ce93cddc71aa0b9459d5b31d
Sam <iaiznura@gmail.com>
2023-08-04 18:36:28 +0300

    add good habit to urls

--- habits/urls.py
+++ habits/urls.py
@@ -1,19 +1,22 @@
 from django.urls import path
 
+from habits.views.good_habit import GoodHabitListView, GoodHabitDetailView, GoodHabitUpdateView, GoodHabitCreateView, \
+    GoodHabitDeleteView
 from habits.views.pleasant_habit import PleasantHabitDetailView, PleasantHabitListView, PleasantHabitUpdateView, \
     PleasantHabitCreateView, PleasantHabitDeleteView
 
 urlpatterns = [
     # PleasantHabit
-    path('', PleasantHabitListView.as_view(), name="pleasant_habit_list"),
-    path('<int:pk>/', PleasantHabitDetailView.as_view(), name="pleasant_habit_detail"),
-    path('<int:pk>/update/', PleasantHabitUpdateView.as_view(), name="pleasant_habit_update"),
-    path('create/', PleasantHabitCreateView.as_view(), name="pleasant_habit_create"),
-    path('<int:pk>/delete/', PleasantHabitDeleteView.as_view(), name="pleasant_habit_delete"),
+    path('pleasant-habit/', PleasantHabitListView.as_view(), name="pleasant_habit_list"),
+    path('pleasant-habit/<int:pk>/', PleasantHabitDetailView.as_view(), name="pleasant_habit_detail"),
+    path('pleasant-habit/<int:pk>/update/', PleasantHabitUpdateView.as_view(), name="pleasant_habit_update"),
+    path('pleasant-habit/create/', PleasantHabitCreateView.as_view(), name="pleasant_habit_create"),
+    path('pleasant-habit/<int:pk>/delete/', PleasantHabitDeleteView.as_view(), name="pleasant_habit_delete"),
 
-
+    # GoodHabit
+    path('good-habit/', GoodHabitListView.as_view(), name="pleasant_habit_list"),
+    path('good-habit/<int:pk>/',  GoodHabitDetailView.as_view(), name="pleasant_habit_detail"),
+    path('good-habit/<int:pk>/update/',  GoodHabitUpdateView.as_view(), name="pleasant_habit_update"),
+    path('good-habit/create/',  GoodHabitCreateView.as_view(), name="pleasant_habit_create"),
+    path('good-habit/<int:pk>/delete/',  GoodHabitDeleteView.as_view(), name="pleasant_habit_delete"),
 ]
-
-
-
-
