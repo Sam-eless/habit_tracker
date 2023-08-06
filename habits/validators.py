aa7cbff5cf7dabb3cb0e79bd9ba1c44c92b64b06
Sam <iaiznura@gmail.com>
2023-08-06 21:39:22 +0300

    fix validators.py

--- habits/validators.py
+++ habits/validators.py
@@ -8,6 +8,8 @@ class ConnectedHabitAndRewardValidator:
         self.field = field
 
     def __call__(self, value):
+        if value.get("reward") is None and value.get("connected_habit") is not None:
+            return True
         if value.get("reward") is not None and value.get("connected_habit") is not None:
             raise serializers.ValidationError(
                 "Невозможно установить награду и приятную привычку одновременно")
@@ -18,8 +20,9 @@ class ExecutionTimeValidator:
         self.field = field
 
     def __call__(self, value):
-        if value.get("execution_time") > time(00, 2, 00):
-            raise serializers.ValidationError("Время выполнения не должно превышать 120 секунд")
+        if value.get("execution_time") is not None:
+            if value.get("execution_time") > time(00, 2, 00):
+                raise serializers.ValidationError("Время выполнения не должно превышать 120 секунд")
 
 
 class FrequencyValidator:
@@ -27,5 +30,6 @@ class FrequencyValidator:
         self.field = field
 
     def __call__(self, value):
-        if value.get("frequency") > 7:
-            raise serializers.ValidationError("Нельзя выполнять привычку реже, чем 1 раз в 7 дней")
+        if value.get("frequency") is not None:
+            if value.get("frequency") > 7:
+                raise serializers.ValidationError("Нельзя выполнять привычку реже, чем 1 раз в 7 дней")
